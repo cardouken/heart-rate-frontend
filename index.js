85ac5eee1fe795339bb692b48a266d297982449c
Cardo Uustal <24594538+cardouken@users.noreply.github.com>
2021-12-07 23:50:14 +0200

    use server-side events, formatting

--- index.js
+++ index.js
@@ -1,15 +1,88 @@
+// const API_BASE_URI = 'http://192.168.1.198/events';
+const API_BASE_URI = 'http://health.uustal.ee/events';
+const source = new EventSource(API_BASE_URI);
 
-const API_BASE_URI = 'http://health.uustal.ee/heart-rate';
-const SMOOTHING_AVG = 2;
-let latestHeartRates = [];
+let data = [];
+let ded = false;
+source.addEventListener('SSE_EVENT', function (e) {
+    console.log("SSE_EVENT " + e.data);
+    data = JSON.parse(e.data);
+}, false);
+source.addEventListener('open', function (e) {
+}, false);
+
+source.addEventListener('error', function (e) {
+    if (e.readyState === EventSource.CLOSED) {
+        ded = true;
+    }
+}, false);
 
 let pfx = ["webkit", "moz", "MS", "o", ""]; // Allow browser prefixes to work for PrefixedEvent
-
 function PrefixedEvent(element, type, callback) { // Allow one JS listener based on browser
     for (let p = 0; p < pfx.length; p++) {
-        if (!pfx[p]) type = type.toLowerCase();
-        element.addEventListener(pfx[p]+type, callback, false);
+        if (!pfx[p]) {
+            type = type.toLowerCase();
+        }
+        element.addEventListener(pfx[p] + type, callback, false);
     }
+}
+
+let hr = 0;
+let rssi = 0;
+let timestamp = 0;
+let hourlyAverage = 0;
+let sixHourAverage = 0;
+
+function animate() {
+    if (ded) {
+        data = undefined;
+    }
+    if (data !== undefined) {
+        hr = data.heartRate;
+        rssi = data.rssi;
+        timestamp = data.timestamp;
+        hourlyAverage = data.hourlyAverage;
+        sixHourAverage = data.sixHourAverage;
+    }
+
+    let $hrate = $('.hrate');
+    let $rssi = $('.rssi');
+    let $avg60min = $('.avg60min');
+    let $avg360min = $('.avg360min');
+
+    const refreshRate = calculateRefreshRate(hr);
+    console.log("ms per beat: " + refreshRate);
+
+    $hrate.text(formatString(hr));
+    $avg60min.html(formatAverage(1, hourlyAverage))
+    $avg360min.html(formatAverage(6, sixHourAverage))
+    $rssi.html(formatRssi(rssi))
+
+    if ($hrate[0] === undefined) {
+        return;
+    }
+    PrefixedEvent($hrate[0], "AnimationIteration", function () { // Apply the listener based on browser
+        let el = $(this),
+            newOne = el.clone(true).css({'animation': 'pulse ' + refreshRate + 'ms infinite'});
+        el.before(newOne);
+        $("." + el.attr("class") + ":last").remove(); // Remove old element
+        animate()
+    });
+}
+
+$(function () {
+    animate();
+})
+
+function calculateRefreshRate(latestHr) {
+    return Math.round(60 / latestHr * 1000);
+}
+
+function formatAverage(time, average) {
+    if (average === undefined) {
+        return "N/A"
+    }
+    return time + "h avg<br>" + average + " bpm";
 }
 
 function formatString(value) {
@@ -19,52 +92,9 @@ function formatString(value) {
     return "❤  " + value + " BPM";
 }
 
-function calculatePulseSpeed(latestHr) {
-    let seconds = 1;
-    if (latestHeartRates.length === SMOOTHING_AVG) {
-        let sum = 0;
-        for (const hr of latestHeartRates) {
-            sum += hr;
-        }
-        seconds = 60 / (sum / SMOOTHING_AVG);
-        latestHeartRates.shift()
-        console.log("Last " + SMOOTHING_AVG + " HR values: " + latestHeartRates + " average: " + Math.round(sum / SMOOTHING_AVG));
+function formatRssi(rssi) {
+    if (rssi === undefined) {
+        return "N/A"
     }
-
-    latestHeartRates.push(latestHr);
-    return seconds;
+    return "rssi<br>" + rssi + " db"
 }
-
-function loop()
-{
-    setTimeout(() => {
-        $.ajax({
-            url: API_BASE_URI,
-            global: false,
-            type: "GET",
-            dataType: "json"
-        }).done(function(data){
-            const pulseInterval = calculatePulseSpeed(data.heartRate);
-            const refreshRate = Math.round(pulseInterval * 1000);
-            let $hrate = $('.hrate');
-
-            console.log(data);
-            console.log("ms per beat: " + refreshRate);
-
-            $hrate.text(formatString(data.heartRate));
-
-            PrefixedEvent($hrate[0], "AnimationIteration", function() { // Apply the listener based on browser
-                let el = $(this),
-                    newOne = el.clone(true).css({'animation': 'pulse ' + refreshRate + 'ms infinite'});
-                el.before(newOne);
-                $("." + el.attr("class") + ":last").remove(); // Remove old element
-
-                loop();
-            });
-        });
-    }, 750);
-}
-
-$(function() {
-    loop();
-})
